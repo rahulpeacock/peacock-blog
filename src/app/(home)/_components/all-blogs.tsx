eafb1ef9a1274ea2e837d3923a5c32a1a24650b1
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-10-04 15:39:09 +0530

    styles: add selection style and all-blogs

--- src/app/(home)/_components/all-blogs.tsx
+++ src/app/(home)/_components/all-blogs.tsx
@@ -19,10 +19,9 @@ export function AllBlogs() {
         <li key={page.file.name} className='flex items-start justify-start group/li hover:bg-[#080808] px-2'>
           <Link
             href={`/${page.slugs.join('/')}`}
-            className='underline lowercase text-tokyo-blue group-hover/li:text-tokyo-green visited:text-tokyo-bmagenta transition-colors duration-150 relative'
+            className='underline lowercase text-tokyo-blue group-hover/li:text-tokyo-green visited:text-tokyo-bmagenta transition-colors duration-150'
           >
             {page.data.title}
-            <span className='group-hover/li:animate-hover-blink opacity-0 bg-tokyo-green inline-block absolute h-5 w-[1ch] -right-3 top-1/2 -translate-y-1/2 pointer-events-none' />
           </Link>
           <div className='text-right group-hover/li:text-tokyo-green flex-grow ml-8 transition-colors duration-150'>
             <time dateTime={page.data.date}>{getDate(page.data.date)}</time>
